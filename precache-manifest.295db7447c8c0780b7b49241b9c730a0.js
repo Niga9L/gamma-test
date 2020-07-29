836b2170f4069c16b047fab03f65ef2bcb5a6a74
Niga9L <lyaginvanya@gmail.com>
2020-07-29 18:52:23 +0300

    Updates

---
+++ precache-manifest.295db7447c8c0780b7b49241b9c730a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ac35e367b3f5fe8c1b4aeaffb2f2a9",
+    "url": "/gamma-test/index.html"
+  },
+  {
+    "revision": "5d46e5831a2910f6e3e2",
+    "url": "/gamma-test/static/css/main.e01412a1.chunk.css"
+  },
+  {
+    "revision": "5c6ae658fc62dfce0273",
+    "url": "/gamma-test/static/js/2.106dcc13.chunk.js"
+  },
+  {
+    "revision": "2bddf808aa595e6c6f0c61a3e69ad57d",
+    "url": "/gamma-test/static/js/2.106dcc13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d46e5831a2910f6e3e2",
+    "url": "/gamma-test/static/js/main.77ab8da7.chunk.js"
+  },
+  {
+    "revision": "2e4a03cfe79269fe50f4",
+    "url": "/gamma-test/static/js/runtime-main.9889d77b.js"
+  }
+]);
